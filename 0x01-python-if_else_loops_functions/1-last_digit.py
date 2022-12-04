5dbfa805c5aef733e691790aaa9eb1531e89d8c2
zeboa <zerabyirga@gmail.com>
2022-12-04 04:41:03 +0300

    Indentation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number < 0:
 print("Last digit of {} is {} and is ".format(number, digit), end="")
 if digit > 5:
     print("greater than 5")
- elif digit == 0:
+elif digit == 0:
     print("0")
 else:
     print("less than 6 and not 0")
