89d3274863834d696bf26feae6ae61d0939fd370
zeboa <zerabyirga@gmail.com>
2022-12-22 08:38:35 +0300

    All at once

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    idx = 0
-    while idx < x:
+    """Print x elememts of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    ret = 0
+    for i in range(x):
         try:
-            print("{}".format(my_list[idx]), end="")
+            print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
             break
-        idx += 1
     print("")
-    return idx
+    return (ret)
---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,5 @@
+g!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
