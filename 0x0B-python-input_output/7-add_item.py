c819243fc0c64cae89980ee355718b39ce0bcc99
zeboa <zerabyirga@gmail.com>
2023-01-10 22:17:15 +0300

    2Finals

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,10 +12,10 @@ Contains function that adds and saves to Python obj to JSON file; loads objects
 # expect output: ["some", "random", "args"]
 """
 
-
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
